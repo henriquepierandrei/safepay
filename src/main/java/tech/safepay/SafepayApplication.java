4c0a15822e992acc26c912739bdad027a76b4933
henriquepierandrei <henriquepierandrei1@gmail.com>
2026-01-13 16:07:30 -0300

    first commit

---
+++ src/main/java/tech/safepay/SafepayApplication.java
@@ -0,0 +1,13 @@
+package tech.safepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafepayApplication.class, args);
+	}
+
+}
